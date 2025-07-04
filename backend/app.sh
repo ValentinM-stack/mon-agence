2ce6a3858b3fdc20806f5ace72f885a48c8f49b1
Valentin <218587221+ValentinM-stack@users.noreply.github.com>
2025-07-04 09:30:53 +0200

    fix app.sh

--- backend/app.sh
+++ backend/app.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
 echo "Backend mon agence - $(date)"
-echo "Script démarré avec PID: $$"
 
-# Simulation d'un travail
-echo "Traitement en cours..."
-sleep 5
-
-echo "Traitement terminé"
+sleep infinity
